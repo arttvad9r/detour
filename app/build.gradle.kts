28bf5eec0078a7d277303872739264377c84d0fe
arttvad9r <arttvad9r@gmail.com>
2026-08-29 00:03:03 +0300

    Wire Navigation Compose into app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(files("../engine/libs/engine.aar"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.navigation.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.material3)
