2842bccf1ac7393cfbdd82116167b020d10e3fdd
arttvad9r <arttvad9r@gmail.com>
2026-09-01 23:25:08 +0300

    Clean brand component imports

--- app/src/main/java/dev/triplet/app/ui/BrandComponents.kt
+++ app/src/main/java/dev/triplet/app/ui/BrandComponents.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
