798c07401b789c50314100b14681edde624ca8f3
arttvad9r <arttvad9r@gmail.com>
2026-09-06 00:41:49 +0300

    test(dpi): scroll proxy test smoke to start action

--- app/src/androidTest/java/dev/triplet/app/DpiProxyTestSmokeTest.kt
+++ app/src/androidTest/java/dev/triplet/app/DpiProxyTestSmokeTest.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.test.junit4.v2.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollTo
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
@@ -29,6 +30,7 @@ class DpiProxyTestSmokeTest {
         rule.onNodeWithText("YouTube").assertIsDisplayed()
         rule.onNodeWithText("GoogleVideo").assertIsDisplayed()
         rule.onNodeWithText(rule.activity.getString(R.string.dpi_proxy_test_start))
+            .performScrollTo()
             .assertIsDisplayed()
     }
 }
